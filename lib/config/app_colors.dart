4a6498489bc3339eae0961f5edbd349840d4d4e6
neit1803 <tien18032002@gmail.com>
2024-08-29 23:20:16 +0700

    App Config Initial

---
+++ lib/config/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class AppColorConfig {
+  static const Color button = Color(0x00925D);
+  static const Color background = Color(0x28262D);
+  static const Color rating = Color(0xFFCD19);
+}
---
+++ lib/config/app_icons.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppIconsConfig {
+  static const Icon searchIcon = Icon(Icons.search);
+  static const Icon ratingIcon = Icon(Icons.star_rate);
+  static const Icon facebookIcon = Icon(Icons.facebook);
+  static const Icon playIcon = Icon(Icons.play_circle);
+  static const Icon pauseIcon = Icon(Icons.pause_circle);
+  static const Icon backwardIcon = Icon(Icons.arrow_back_ios_new);
+  static const Icon forwardIcon = Icon(Icons.arrow_forward_ios);
+  static const Icon favoriteIcon = Icon(Icons.bookmark_border_outlined);
+  static const Icon modifyIcon = Icon(Icons.camera_alt_outlined);
+  static const Icon notifcationIcon = Icon(Icons.notifications_none_outlined);
+}
